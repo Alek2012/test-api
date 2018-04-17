b5a8d3577626e34dc9c2e738ba2fa6d54887b4b5
Alex Timkov <itdepartment7@gmail.com>
2018-04-17 11:23:45 +0300

    ReposSeeder added

--- app/Repo.php
+++ app/Repo.php
@@ -4,6 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Repo
+ *
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $github_user
+ * @property string $title
+ * @property string $repo_url
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Repo whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Repo whereGithubUser($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Repo whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Repo whereRepoUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Repo whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Repo whereUpdatedAt($value)
+ */
 class Repo extends Model
 {
     protected $fillable = ['title', 'github_user', 'repo_url'];
---
+++ database/seeds/ReposTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Repo;
+
+class ReposTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        {
+            // Let's truncate our existing records to start from scratch.
+            Repo::truncate();
+
+            $faker = \Faker\Factory::create();
+
+            // And now, let's create a few articles in our database:
+            for ($i = 0; $i < 1000; $i++) {
+                Repo::create([
+                    'github_user' => $faker->name,
+                    'title' => $faker->sentence,
+                    'repo_url' => $faker->url,
+                ]);
+            }
+        }
+    }
+}
